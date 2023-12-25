93037d538af83f07e8ff74f01b9c1ce552a91c3f
jakharsapna <139456735+jakharsapna@users.noreply.github.com>
2023-12-25 21:53:32 +0530

    my-calculator

---
+++ style.js
@@ -0,0 +1,19 @@
+let string=""
+let input=document.getElementById('input')
+let buttons=document.querySelectorAll('.button')
+
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+               string=eval(string)
+               input.value=string
+        }else if(e.target.innerHTML=='AC'){
+            string=""
+            input.value=string
+        }
+        else{
+            string=string+e.target.innerHTML
+            input.value=string
+        }
+    })
+})
